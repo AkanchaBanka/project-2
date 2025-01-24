3dbf04f20ce482f1026e18af4ea13bf78b81b926
AkanchaBanka <akancha.banka8@gmail.com>
2025-01-23 22:46:36 -0800

    cra and taildwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      Let's build my second project
+    </div>
+  );
+}
+
+export default App;
